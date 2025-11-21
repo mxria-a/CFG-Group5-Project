34f6cfa63e2acc13055524b4fe559bf1240a10b5
Jescintha Rajkumar <jescintha.r@gmail.com>
2025-11-21 09:49:27 +0000

    feat: Add Jescintha Rajkumar's introduction

--- group5app/src/App.jsx
+++ group5app/src/App.jsx
@@ -12,7 +12,12 @@ function App() {
         <p><b>Favourite hobby:</b> Binging reality shows</p>
         <p>I do this because it's my favourite thing to do apart from sleeping</p>
         <div className="divider"></div>
-      </header>
+      
+    <h2 className='Teammate-name'>Jescintha Rajkumar</h2>
+<p><b>Favourite hobby:</b> Reading</p>
+<p>I love reading as a student of literature and language, exploring diverse narratives and linguistic structures.</p>
+<div className="divider"></div>
+</header>
     </div>
   );
 }
