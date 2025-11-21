738e3c08ad7c3b28a243b11eab697affdb8db03b
Jess Branch <jessbranch@hotmail.co.uk>
2025-11-21 11:18:10 +0000

    Jess adding about-me to the group introductions

--- group5app/src/App.jsx
+++ group5app/src/App.jsx
@@ -1,23 +1,39 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='Introductions-header'>
-          Team Introductions
-        </h1>
-        <h2 className='Teammate-name'>Maria</h2>
-        <p><b>Favourite hobby:</b> Binging reality shows</p>
-        <p>I do this because it's my favourite thing to do apart from sleeping</p>
+        <h1 className="Introductions-header">Team Introductions</h1>
+        <h2 className="Teammate-name">Maria</h2>
+        <p>
+          <b>Favourite hobby:</b> Binging reality shows
+        </p>
+        <p>
+          I do this because it's my favourite thing to do apart from sleeping
+        </p>
         <div className="divider"></div>
-      
-    <h2 className='Teammate-name'>Jescintha Rajkumar</h2>
-<p><b>Favourite hobby:</b> Reading</p>
-<p>I love reading as a student of literature and language, exploring diverse narratives and linguistic structures.</p>
-<div className="divider"></div>
-</header>
+
+        <h2 className="Teammate-name">Jescintha Rajkumar</h2>
+        <p>
+          <b>Favourite hobby:</b> Reading
+        </p>
+        <p>
+          I love reading as a student of literature and language, exploring
+          diverse narratives and linguistic structures.
+        </p>
+        <div className="divider"></div>
+        <h2 className="Teammate-name">Jess</h2>
+        <p>
+          <b>Favourite hobby:</b> Cycling
+        </p>
+        <p>
+          I love being outside: cycling, walking, running. But I also love
+          watching trash TV on the sofa.
+        </p>
+        <div className="divider"></div>
+      </header>
     </div>
   );
 }
