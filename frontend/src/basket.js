936ab975691e648f9099f83b606af59ff9e7199a
temio <temioyelade8@gmail.com>
2025-12-04 20:50:53 +0000

    created divs for basket and items

--- frontend/src/basket.js
+++ frontend/src/basket.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const basket = () => {
-  return <div>basket</div>;
+  const { basketItems, Foodlist, removeFromBasket } = useContext(StoreContext);
+
+  return (
+    <div class="basket">
+      <div class="basket-items">
+        <div class="basket-items-name">
+          <p>Items</p>
+          <p>Name</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default basket;
