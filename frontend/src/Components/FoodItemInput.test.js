9533435b8cae6262b3f5932d6e492f6cf98943a3
Chantelle Bailey <chantelle8@live.co.uk>
2025-12-10 22:25:21 +0000

    continued adding test code to simulate what real user would do when selcting option from dropdown

--- frontend/src/Components/FoodItemInput.test.js
+++ frontend/src/Components/FoodItemInput.test.js
@@ -1,17 +1,22 @@
 import {render, screen} from "@testing-library/react";
-import userEvent from "@testing-library/userevent";
-import GetItem from "./GetItem"; 
+import userEvent from "@testing-library/user-event";
+import FoodItemInput from "./FoodItemInput";
+
 
 //create user and mock function
-test("renders autocomplete and selects an option", async () => {
+test("should call setItem with the selected value when an option is clikced", async () => {
     const user = userEvent.setup();
     const setItem = jest.fn();
 
 //load react component 
-render(<GetItem item="" setItem={setItem} />);
+render(<FoodItemInput  item="" setItem={setItem} />);
 
 //find input field where user types
 const input = screen.getByLabelText(/what are you craving/i); 
 
+//activity of opening drop down and selecting option 
+await user.click(input); 
+await user.click(screen.getByText("Burger"));
 
-})
+expect(setItem).toHaveBeenCalledWith("Burger");
+}); 
