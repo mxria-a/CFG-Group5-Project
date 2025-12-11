8f181c3e79f53fecadb5fbc76883ae92f0cac661
Maria Ameyinoge <maria.ameey@gmail.com>
2025-12-11 01:47:40 +0000

    created a route for allergens

---
+++ backend/routes/allergens.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const pool = require("../db");
+const router = express.Router();
+
+// Creating route to get allergen displayed in dropdown
+router.get("/:id", (req, res) => {
+  const sql = "SELECT * FROM allergens";
+  pool.query(sql, (err, results) => {
+    if (err) {
+      console.error("Database error fetching allergens:", err);
+      return res.status(500).json({ error: "Database error fetching allergens" });
+    }
+    res.status(200).json(results);
+  });
+});
+
+module.exports = router;
