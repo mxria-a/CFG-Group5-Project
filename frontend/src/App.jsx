ca0506f0efc4b900d687aa56cf4d41c195360e99
Jescintha Rajkumar <jescintha.r@gmail.com>
2025-12-11 22:13:02 +0000

    added a footer will be modifying it later

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,47 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Box } from "@mui/material"; // 
 import "./App.css";
 import Home from "./Pages/Homepage";
 import TeamIntro from "./Components/TeamIntro";
+import Footer from "./Components/Footer"; // 
 
 function App() {
   return (
     <BrowserRouter>
-      <nav className="navbar">
-        <div className="nav-logo">App name</div>
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/basket">Basket</Link>
-          <Link to="/profile">Your Account</Link>
-          <Link to="/about-us">About Us</Link>
-        </div>
-      </nav>
+      {/* wrapped everything in this Box */}
+      {/* This creates the sticky footer layout */}
+      <Box 
+        sx={{ 
+          display: "flex", 
+          flexDirection: "column", 
+          minHeight: "100vh" 
+        }}
+      >
+        
+        {/*  Navbar */}
+        <nav className="navbar">
+          <div className="nav-logo">App name</div>
+          <div className="nav-links">
+            <Link to="/">Home</Link>
+            <Link to="/basket">Basket</Link>
+            <Link to="/profile">Your Account</Link>
+            <Link to="/about-us">About Us</Link>
+          </div>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" />
-        <Route path="/about-us" element={<TeamIntro />} />
-      </Routes>
+        {/* Main Content Area - flexGrow pushes the footer down */}
+        <Box component="main" sx={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" />
+            <Route path="/about-us" element={<TeamIntro />} />
+          </Routes>
+        </Box>
+
+        {/* The Footer */}
+        <Footer />
+
+      </Box>
     </BrowserRouter>
   );
 }
