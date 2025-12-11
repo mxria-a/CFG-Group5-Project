e5a08e90271504dd8d7fe71aa7b0c7c83c78ff37
Maria Ameyinoge <maria.ameey@gmail.com>
2025-12-11 02:04:19 +0000

    corrected spelling mistake

--- backend/routes/allergens.js
+++ backend/routes/allergens.js
@@ -2,16 +2,16 @@ const express = require("express");
 const pool = require("../db");
 const router = express.Router();
 
-// Creating route to get allergen displayed in dropdown
-router.get("/:id", (req, res) => {
-  const sql = "SELECT * FROM allergens";
-  pool.query(sql, (err, results) => {
-    if (err) {
-      console.error("Database error fetching allergens:", err);
-      return res.status(500).json({ error: "Database error fetching allergens" });
-    }
-    res.status(200).json(results);
+// Creating route to get allergens displayed in dropdown
+router.get("/", (req, res) => {
+    const sql = "SELECT * FROM allergens";
+    pool.query(sql, (err, results) => {
+      if (err) {
+        console.error("Database error fetching allergens:", err);
+        return res.status(500).json({ error: "Database error fetching allergens" });
+      }
+      res.status(200).json(results);
+    });
   });
-});
 
 module.exports = router;
