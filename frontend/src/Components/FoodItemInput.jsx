f5eaa8a6e33f1a7796b1f2bf95c7d385a7dae867
Chantelle Bailey <chantelle8@live.co.uk>
2025-12-13 23:43:37 +0000

    Added styling to titles and layout to make it centered and seperated into sections with some added effects

--- frontend/src/Components/FoodItemInput.jsx
+++ frontend/src/Components/FoodItemInput.jsx
@@ -9,19 +9,45 @@ function GetItem({ item, setItem }) {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        width: "100%",
+        paddingTop: "40px",
       }}
     >
       <Autocomplete
         options={OPTIONS}
-        
-        // controlled input
         value={item || ""}
-
         onChange={(_, newItem) => setItem(newItem || "")}
-
-        sx={{ width: 300, backgroundColor: "white" }}
+        sx={{
+          width: 350,
+          "& .MuiOutlinedInput-root": {
+            backgroundColor: "#ffffff",        
+            borderRadius: "14px",            
+            boxShadow: "0 4px 12px rgba(0,0,0,0.06)", 
+            "& fieldset": {
+              borderColor: "#ffd1b3",         
+            },
+            "&:hover fieldset": {
+              borderColor: "#ff6b35",          
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "#ff6b35",          
+            },
+          },
+          "& .MuiAutocomplete-endAdornment": {
+            color: "#ff6b35",                
+          },
+        }}
         renderInput={(params) => (
-          <TextField {...params} label="What are you craving?" />
+          <TextField
+            {...params}
+            label="What are you craving?"
+            sx={{
+              "& .MuiInputLabel-root": { color: "#111111ff" }, 
+              "& .MuiOutlinedInput-input": {
+                color: "#333",                              
+              },
+            }}
+          />
         )}
       />
     </Box>
